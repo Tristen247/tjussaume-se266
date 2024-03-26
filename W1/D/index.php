fc74d5eefead646ca91265c81135887de02f7550
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2024-03-25 22:09:29 -0400

    Part D finished

---
+++ W1/D/index.php
@@ -0,0 +1,21 @@
+<?php 
+# associative array named 
+$task = [
+    'title' => 'W1 mini task',
+    'due' => 'friday',
+    'assigned_to' => 'me',
+    'completed' => 'yes'
+];
+
+require 'index.view.php'
+
+/*
+echo '<pre>'; # 'pre' wrap for quick formatting
+var_dump($person); #print an array in this case or anything 
+echo '<pre>';
+
+die(); <-- to stop executing this line 
+
+unset(); <-- remove a value/key pair
+*/
+?>
