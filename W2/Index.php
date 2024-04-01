e74223165411a35e106c3a2fd250222fb799c3ee
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2024-04-01 09:23:16 -0400

    added w2 folder for patient intake

--- W2/Index.php
+++ W2/Index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php
+#include the nessecary files
+require 'functions.php';
+require 'index.view.php';
 
-<form action="post"></form>
-
-function calcStandings($wins, $otLosses){
-
-    $points = ($wins*2, otLosses+)
-
-}
-    
-</body>
-</html>
+?>
---
+++ W2/index.view.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Patient Intake Form</h1>
+
+    <form action="post" name="patientIntake">
+        <div class="container">
+            <div class="label">
+                <label for="fName">First Name: </label>
+            </div>
+            <div>
+                <input type="text" name="name">
+            </div>
+            <div class="label">
+                <label for="lName">Last Name: </label>
+            </div>
+            <div>
+                <input type="text" name="name">
+            </div>
+            <div class="label">
+                <p>Married?</p>
+                    <input type="radio" id="yes" name="married" value="yes">
+                <label for="married">Yes</label><br>
+                    <input type="radio" id="no" name="married" value="no">
+                <label for="married">No</label><br>
+            </div>
+                
+          
+
+
+
+
+
+
+        </div>
+
+    </form>
+    
+</body>
+</html>
