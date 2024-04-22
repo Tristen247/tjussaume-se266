f1d45c15be2b439b90bef3d5eb42a4b1dd47b2b5
Tristen Jussaume <tristenjussaume@gmail.com>
2024-04-21 20:03:05 -0400

    Created DB Functions

--- class_website/w4/db.php
+++ class_website/w4/db.php
@@ -1,11 +1,11 @@
 <?php
 $ini = parse_ini_file('dbconfig.ini');
 
-$db = new PDO("mysql:host=" . $ini[''] . 
-              ";port=" . $ini['port'] . 
-              ";dbname=" . $ini['dbname'], 
-              $ini['username'], 
-              $ini['password']);
+$db = new PDO(  "mysql:host=" . $ini['servername'] . 
+    ";port=" . $ini['port'] . 
+    ";dbname=" . $ini['dbname'], 
+    $ini['username'], 
+    $ini['password']);
 
-            
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);    
 ?>
--- class_website/w4/model_patients.php
+++ class_website/w4/model_patients.php
@@ -1,16 +1,48 @@
-<?php include __DIR__ . '/db.php'; ?>
 <?php
 
+include __DIR__ . '/db.php'; 
 
-var_dump($db);
-exit;
 
 function GetPatients() {
 
     global $db;
 
+    $results = [];
 
+    $stmt = $db->prepare("SELECT id, patientFirstName, patientLastName, patientMarried FROM patients ORDER BY id;");
 
+    if ($stmt->execute() && $stmt->rowCount() > 0 ) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    return ($results);
+    
 
 }
+
+function addPatient ($fName, $lName, $married) {
+    global $db;
+
+    
+    $stmt = $db->prepare("INSERT INTO patients set patientFirstName = :patientFirstName, patientLastName = :patientLastName, patientMarried = 0");
+    $binds = array(
+        ":patientFirstName" => $fName,
+        ":patientLastName" => $lName,
+        ":patientMarried" => $married,
+    );
+
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0){
+        $results = "Data Added";
+    }
+
+    return ($results);
+}
+
+$patients = GetPatients();
+$patient = $patients[0];
+echo $patient["patientFirstName"];
+
+
+
+
 ?>
