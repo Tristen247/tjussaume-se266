7a7063a40caeb69cd62bda872bbeedffd693f5eb
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2024-05-20 09:30:25 -0400

    final

--- Game-Review-Website-PHP-Final/public/index.php
+++ Game-Review-Website-PHP-Final/public/index.php
@@ -1,1 +1,14 @@
-<?php include __DIR__ . '../templates/header.php'; ?>
+<!-- --><?php include __DIR__ . '/../templates/header.php'; ?>
+
+
+
+
+
+
+
+<?php include __DIR__ . '/../templates/footer.php'; ?>
+
+
+
+
+
--- Game-Review-Website-PHP-Final/templates/header.php
+++ Game-Review-Website-PHP-Final/templates/header.php
@@ -4,40 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- Bootstrap V5.3-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <!--Font awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+        <!--My CSS-->
+        <link href="./assets/style.css" rel="stylesheet">
 </head>
 <body>
-    
-        <nav class="navbar navbar-expand-sm navbar-light bg-primary fixed-top">
-            <a class="navbar-brand" href="../site/index.php">
-            <img src="../images/hosp-logo.png" width="40" height="40" alt="">
-            </a>
-            <button
-                class="navbar-toggler d-lg-none"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#collapsibleNavId"
-                aria-controls="collapsibleNavId"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            ></button>
-            <div class="collapse navbar-collapse" id="collapsibleNavId">
-                <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active text-white" href="./patients.view.php" aria-current="page">
-                            View Patients <span class="visually-hidden">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active text-white" href="./search.php" aria-current="page">
-                            Search Patient <span class="visually-hidden">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active text-white" href="https://github.com/Tristen247/tjussaume-se266/blob/f3c459fb263921fa19041689d84d9ef63435306e/class_website/w2/index.php" aria-current="page">
-                            GitHub<span class="visually-hidden">(current)</span></a>
-                    </li>
-                </ul>
-                <a href="your_facebook_link" class="social-icon"><i class="fab fa-facebook-f"></i></a>
-                <a href="your_twitter_link" class="social-icon"><i class="fab fa-twitter"></i></a>
-                <a href="your_instagram_link" class="social-icon"><i class="fab fa-instagram"></i></a>
-                <a href="your_github_profile_link" class="social-icon"><i class="fab fa-github"></i></a>
-            </div>
-        </nav>
+        <div class="wrapper">
+            <main class="content">
+                <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+                    <a class="navbar-brand" href="index.php">
+                    <img src="../images/hosp-logo.png" width="40" height="40" alt="">
+                    </a>
+                    <button
+                        class="navbar-toggler d-lg-none"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapsibleNavId"
+                        aria-controls="collapsibleNavId"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    ></button>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active text-white" href="#####" aria-current="page">
+                                    Home <span class="visually-hidden">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active text-white" href="./search.php" aria-current="page">
+                                    Search Games <span class="visually-hidden">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active text-white" href="https://github.com/Tristen247/tjussaume-se266/blob/f3c459fb263921fa19041689d84d9ef63435306e/class_website/w2/index.php" aria-current="page">
+                                    GitHub<span class="visually-hidden">(current)</span></a>
+                            </li>
+                        </ul>
+                        <a href="your_facebook_link" class="social-icon"><i class="fab fa-facebook-f"></i></a>
+                        <a href="your_twitter_link" class="social-icon"><i class="fab fa-twitter"></i></a>
+                        <a href="your_instagram_link" class="social-icon"><i class="fab fa-instagram"></i></a>
+                        <a href="your_github_profile_link" class="social-icon"><i class="fab fa-github"></i></a>
+                    </div>
+                </nav>
+            </main>
